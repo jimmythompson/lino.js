a76a9dc34b4338ff4d6cf88c8884ff7832435ab5
jimmythompson <jimmy@freetrade.io>
2019-11-09 23:08:15 +0000

    Linting

--- src/CommandLine.ts
+++ src/CommandLine.ts
@@ -20,90 +20,90 @@ interface ExecutionResult {
 }
 
 export class CommandLine {
-    static forCommand(application: string): CommandLine {
-        return new CommandLine(application)
-    }
+  static forCommand (application: string): CommandLine {
+    return new CommandLine(application)
+  }
 
-    private constructor(
+  private constructor (
         private application: string,
         private environmentVariables: Option[] = [],
         private flags: string[] = [],
         private options: Option[] = [],
         private commandArguments: string[] = [],
         private separator: string = ' '
-    ) {
-    }
+  ) {
+  }
 
-    public withEnvironmentVariable(key: string, value: string): CommandLine {
-        this.environmentVariables.push({
-            key,
-            value,
+  public withEnvironmentVariable (key: string, value: string): CommandLine {
+    this.environmentVariables.push({
+      key,
+      value
+    })
+    return this
+  }
+
+  public withFlag (flag: string): CommandLine {
+    this.flags.push(flag)
+    return this
+  }
+
+  public withOption (key: string, value: string, config: OptionConfig = {}): CommandLine {
+    this.options.push({
+      key,
+      value,
+      ...(config.separator ? { separator: config.separator } : {})
+    })
+    return this
+  }
+
+  public withOptionSeparator (separator: string): CommandLine {
+    this.separator = separator
+    return this
+  }
+
+  public withArgument (argument: string, config: ArgumentConfig = { wrap: false }): CommandLine {
+    const processedArgument = config.wrap
+      ? `"${argument}"`
+      : argument
+
+    this.commandArguments.push(processedArgument)
+    return this
+  }
+
+  public toString (): string {
+    const combinedEnvironmentVariables = this.environmentVariables.reduce((acc, next) => {
+      return [...acc, `${next.key}=${next.value}`]
+    }, []).join(' ')
+    const combinedFlags = this.flags.join(' ')
+    const combinedOptions = this.options.reduce((acc, next) => {
+      const separator = next.separator || this.separator
+      return [...acc, `${next.key}${separator}${next.value}`]
+    }, []).join(' ')
+    const combinedArguments = this.commandArguments.join(' ')
+
+    return [
+      combinedEnvironmentVariables,
+      this.application,
+      combinedFlags,
+      combinedOptions,
+      combinedArguments
+    ]
+      .filter(segment => segment.length > 0)
+      .join(' ')
+  }
+
+  public execute (): Promise<ExecutionResult> {
+    return new Promise((resolve, reject) => {
+      exec(this.toString(), (error, stdout, stderr) => {
+        if (error) {
+          return reject(error)
+        }
+
+        return resolve({
+          stdout: stdout.trim(),
+          stderr: stderr.trim()
         })
-        return this
-    }
-
-    public withFlag(flag: string): CommandLine {
-        this.flags.push(flag)
-        return this
-    }
-
-    public withOption(key: string, value: string, config: OptionConfig = {}): CommandLine {
-        this.options.push({
-            key,
-            value,
-            ...(config.separator ? { separator: config.separator } : {})
-        })
-        return this
-    }
-
-    public withOptionSeparator(separator: string): CommandLine {
-        this.separator = separator
-        return this
-    }
-
-    public withArgument(argument: string, config: ArgumentConfig = { wrap: false }): CommandLine {
-        const processedArgument = config.wrap
-            ? `"${argument}"`
-            : argument
-
-        this.commandArguments.push(processedArgument)
-        return this
-    }
-
-    public toString(): string {
-        const combinedEnvironmentVariables = this.environmentVariables.reduce((acc, next) => {
-            return [...acc, `${next.key}=${next.value}`]
-        }, []).join(' ')
-        const combinedFlags = this.flags.join(' ')
-        const combinedOptions = this.options.reduce((acc, next) => {
-            const separator = next.separator || this.separator
-            return [...acc, `${next.key}${separator}${next.value}`]
-        }, []).join(' ')
-        const combinedArguments = this.commandArguments.join(' ')
-
-        return [
-            combinedEnvironmentVariables,
-            this.application,
-            combinedFlags,
-            combinedOptions,
-            combinedArguments
-        ]
-            .filter(segment => segment.length > 0)
-            .join(' ')
-    }
-
-    public execute(): Promise<ExecutionResult> {
-        return new Promise((resolve, reject) => {
-            exec(this.toString(), (error, stdout, stderr) => {
-                if (error) {
-                    return reject(error)
-                }
-
-                return resolve({
-                    stdout: stdout.trim(),
-                    stderr: stderr.trim()
-                })
-            })
-        })
-    }
+      })
+    })
+  }
 }
--- test/CommandLine.spec.ts
+++ test/CommandLine.spec.ts
@@ -1,131 +1,131 @@
 import { CommandLine } from '../src/CommandLine'
 
 describe('CommandLine', () => {
-    it('turns a command into a string', () => {
-        expect(CommandLine.forCommand('node').toString())
-            .toEqual('node')
-    })
+  it('turns a command into a string', () => {
+    expect(CommandLine.forCommand('node').toString())
+      .toEqual('node')
+  })
 
-    it('can assemble complex commands', () => {
-        let commandLine =
+  it('can assemble complex commands', () => {
+    const commandLine =
             CommandLine.forCommand('command-with-options')
-                .withEnvironmentVariable('LOCAL', 'true')
-                .withFlag('-v')
-                .withOption('--opt1', 'val1')
-                .withArgument('path/to/file.txt')
+              .withEnvironmentVariable('LOCAL', 'true')
+              .withFlag('-v')
+              .withOption('--opt1', 'val1')
+              .withArgument('path/to/file.txt')
 
-        expect(commandLine.toString())
-            .toEqual('LOCAL=true command-with-options -v --opt1 val1 path/to/file.txt')
+    expect(commandLine.toString())
+      .toEqual('LOCAL=true command-with-options -v --opt1 val1 path/to/file.txt')
+  })
+
+  describe('withOption', () => {
+    it('includes options after the command separated by a space', () => {
+      const commandLine =
+                CommandLine.forCommand('command-with-options')
+                  .withOption('--opt1', 'val1')
+                  .withOption('--opt2', 'val2')
+
+      expect(commandLine.toString())
+        .toEqual('command-with-options --opt1 val1 --opt2 val2')
     })
 
-    describe('withOption', () => {
-        it('includes options after the command separated by a space', () => {
-            let commandLine =
+    it('includes options with a custom separator', () => {
+      const commandLine =
                 CommandLine.forCommand('command-with-options')
-                    .withOption('--opt1', 'val1')
-                    .withOption('--opt2', 'val2')
+                  .withOptionSeparator('=')
+                  .withOption('--opt1', 'val1')
+                  .withOption('--opt2', 'val2')
 
-            expect(commandLine.toString())
-                .toEqual('command-with-options --opt1 val1 --opt2 val2')
-        })
-
-        it('includes options with a custom separator', () => {
-            let commandLine =
-                CommandLine.forCommand('command-with-options')
-                    .withOptionSeparator('=')
-                    .withOption('--opt1', 'val1')
-                    .withOption('--opt2', 'val2')
-
-            expect(commandLine.toString())
-                .toEqual('command-with-options --opt1=val1 --opt2=val2')
-        })
-
-        it('includes options with mixed separators', () => {
-            let commandLine =
-                CommandLine.forCommand('command-with-options')
-                    .withOption('--opt1', 'val1', { separator: '=' })
-                    .withOption('--opt2', 'val2')
-
-            expect(commandLine.toString())
-                .toEqual('command-with-options --opt1=val1 --opt2 val2')
-        })
-
-        it('includes options with a custom separator and mixed separators', () => {
-            let commandLine =
-                CommandLine.forCommand('command-with-options')
-                    .withOptionSeparator('=')
-                    .withOption('--opt1', 'val1', { separator: ' ' })
-                    .withOption('--opt2', 'val2')
-
-            expect(commandLine.toString())
-                .toEqual('command-with-options --opt1 val1 --opt2=val2')
-        })
+      expect(commandLine.toString())
+        .toEqual('command-with-options --opt1=val1 --opt2=val2')
     })
 
-    describe('withFlag', () => {
-        it('includes flags after the command', () => {
-            let commandLine =
+    it('includes options with mixed separators', () => {
+      const commandLine =
                 CommandLine.forCommand('command-with-options')
-                    .withFlag('--verbose')
-                    .withFlag('-h')
+                  .withOption('--opt1', 'val1', { separator: '=' })
+                  .withOption('--opt2', 'val2')
 
-            expect(commandLine.toString())
-                .toEqual('command-with-options --verbose -h')
-        })
+      expect(commandLine.toString())
+        .toEqual('command-with-options --opt1=val1 --opt2 val2')
     })
 
-    describe('withArgument', () => {
-        it('includes arguments after the command', () => {
-            let commandLine =
+    it('includes options with a custom separator and mixed separators', () => {
+      const commandLine =
                 CommandLine.forCommand('command-with-options')
-                    .withArgument('path/to/file.txt')
+                  .withOptionSeparator('=')
+                  .withOption('--opt1', 'val1', { separator: ' ' })
+                  .withOption('--opt2', 'val2')
 
-            expect(commandLine.toString())
-                .toEqual('command-with-options path/to/file.txt')
-        })
+      expect(commandLine.toString())
+        .toEqual('command-with-options --opt1 val1 --opt2=val2')
+    })
+  })
 
-        it('wraps in quotes when specified', () => {
-            let commandLine =
+  describe('withFlag', () => {
+    it('includes flags after the command', () => {
+      const commandLine =
                 CommandLine.forCommand('command-with-options')
-                    .withArgument('path/to/file.txt', { wrap: true })
+                  .withFlag('--verbose')
+                  .withFlag('-h')
 
-            expect(commandLine.toString())
-                .toEqual('command-with-options "path/to/file.txt"')
-        })
+      expect(commandLine.toString())
+        .toEqual('command-with-options --verbose -h')
+    })
+  })
+
+  describe('withArgument', () => {
+    it('includes arguments after the command', () => {
+      const commandLine =
+                CommandLine.forCommand('command-with-options')
+                  .withArgument('path/to/file.txt')
+
+      expect(commandLine.toString())
+        .toEqual('command-with-options path/to/file.txt')
     })
 
-    describe('withEnvironmentVariable', () => {
-        it('includes environment variables before the command', () => {
-            let commandLine =
+    it('wraps in quotes when specified', () => {
+      const commandLine =
                 CommandLine.forCommand('command-with-options')
-                    .withEnvironmentVariable('ENV_VAR1', 'VAL1')
-                    .withEnvironmentVariable('ENV_VAR2', 'VAL2')
+                  .withArgument('path/to/file.txt', { wrap: true })
 
-            expect(commandLine.toString())
-                .toEqual('ENV_VAR1=VAL1 ENV_VAR2=VAL2 command-with-options')
-        })
+      expect(commandLine.toString())
+        .toEqual('command-with-options "path/to/file.txt"')
     })
+  })
 
-    describe('execute', () => {
-        it('executes the command', async () => {
-            let commandLine =
+  describe('withEnvironmentVariable', () => {
+    it('includes environment variables before the command', () => {
+      const commandLine =
+                CommandLine.forCommand('command-with-options')
+                  .withEnvironmentVariable('ENV_VAR1', 'VAL1')
+                  .withEnvironmentVariable('ENV_VAR2', 'VAL2')
+
+      expect(commandLine.toString())
+        .toEqual('ENV_VAR1=VAL1 ENV_VAR2=VAL2 command-with-options')
+    })
+  })
+
+  describe('execute', () => {
+    it('executes the command', async () => {
+      const commandLine =
                 CommandLine.forCommand('echo')
-                    .withArgument('Hello, world!', { wrap: true })
+                  .withArgument('Hello, world!', { wrap: true })
 
-            let result = await commandLine.execute()
+      const result = await commandLine.execute()
 
-            expect(result.stdout).toEqual("Hello, world!")
-        })
-
-        it('executes the command', async () => {
-            let commandLine =
-                CommandLine.forCommand('echo')
-                    .withArgument('Hello, world!', { wrap: true })
-                    .withArgument('>&2')
-
-            let result = await commandLine.execute()
-
-            expect(result.stderr).toEqual("Hello, world!")
-        })
+      expect(result.stdout).toEqual('Hello, world!')
     })
+
+    it('executes the command', async () => {
+      const commandLine =
+                CommandLine.forCommand('echo')
+                  .withArgument('Hello, world!', { wrap: true })
+                  .withArgument('>&2')
+
+      const result = await commandLine.execute()
+
+      expect(result.stderr).toEqual('Hello, world!')
+    })
+  })
 })
